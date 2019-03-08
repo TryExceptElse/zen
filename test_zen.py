a2dea1d176abe942b26ced6a395c71f325c2ca7d
TryExceptElse <schwarzjohnv@gmail.com>
2019-03-07 21:18:52 -0800

    Tests now actually pass on 64 bit systems again

--- test_zen.py
+++ test_zen.py
@@ -1184,7 +1184,7 @@ class TestFindScopeTokens(TestCase):
 class TestIterHash(TestCase):
     def test_hash_is_repeatable(self):
         result: int = zen.iter_hash((s for s in ['a', 'b', 'c']))
-        if sys.maxsize == 2 ** 31 - 1
+        if sys.maxsize == 2 ** 63 - 1:
             self.assertEqual(8304879420899386742, result)
         else:
             self.assertEqual(2082300178, result)
